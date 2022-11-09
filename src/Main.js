8b8e86649d2022d2a3cca3c8bd1ee8f59e4ad21d
Ricardo Colom <ricardo.colom@kaseya.com>
2022-11-09 10:25:54 -0500

    Skill info as a table

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,50 @@
-import { Avatar, Box, Text, HStack, VStack, AvatarBadge } from '@chakra-ui/react'
 import React from 'react'
-import { MdCake, MdEmail } from 'react-icons/md'
+import { Avatar, Box, Text, HStack, VStack, AvatarBadge, Code, Button, Tooltip, IconButton } from '@chakra-ui/react'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+} from '@chakra-ui/react'
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
+import { MdCake, MdOutlineDelete, MdOutlineEdit, MdEmail } from 'react-icons/md'
 
 const today = new Date();
 
 const employee1DOB = new Date("5/1/2001");
 const employee2DOB = new Date("4/12/1998");
+
+const employee1skills = [
+    {
+        name: "Front-End",
+        id: "000",
+        desc: "aaaaaaaaa"
+    },
+    {
+        name: "Back-End",
+        id: "001",
+        desc: "bbbbbbbbb"
+    }
+]
+
+const employee2skills = [
+    {
+        name: "Communications",
+        id: "003",
+        desc: "aaaaaaaaa"
+    },
+]
 
 const RenderActivity = (isActive) => {
     if (isActive) {
@@ -20,15 +59,53 @@ const GetAge = (dob) => {
     return Math.floor(currAge);
 }
 
+const SkillBlock = (props) => {
+    
+}
+
 const EmployeeCard = (props) => {
+
+    const skillNames = [];
+    (props.skills).forEach(skill => {
+        skillNames.push(skill.name);
+    });
+
+    console.log(skillNames)
+
     return (
         <Box display="flex" justifyContent="center" m="2">
-            <Box w="400px" h="256px" maxW="sm" borderWidth="1px" borderRadius="2xl" bg="white">
-                <Box pos="absolute" right="4" m="2">
+            <Box pos="relative" w="md" maxW="lg" borderWidth="1px" borderRadius="2xl" bg="white">
+                <Box pos="absolute" m="2" right="0">
+                    <HStack>
+                        <IconButton
+                            colorScheme='gray'
+                            aria-label='Edit Employee'
+                            size="sm"
+                            icon={<MdOutlineEdit fontSize="16pt" color="black" />}
+                            variant="ghost"
+                        />
+                        <IconButton
+                            colorScheme='red'
+                            aria-label='Delete Employee'
+                            size="sm"
+                            icon={<MdOutlineDelete fontSize="16pt" color="black" />}
+                            variant="ghost"
+                        />
+                    </HStack>
+
+                </Box>
+                <Box pos="absolute" m="2" right="0" bottom="0">
                     <HStack>
                         <Text color="gray">
-                            ID: {props.id}
+                            ID:
                         </Text>
+                        <Tooltip label="Click to copy" borderRadius="lg">
+                            <Button size="xs" onClick={() => navigator.clipboard.writeText(props.id)}>
+                                <Code bg="transparent">
+                                    {props.id}
+                                </Code>
+                            </Button>
+                        </Tooltip>
                     </HStack>
                 </Box>
                 <Box m="4">
@@ -64,6 +141,41 @@ const EmployeeCard = (props) => {
                             </HStack>
                         </HStack>
                     </VStack>
+
+                    <Accordion allowToggle pb="8">
+                        <AccordionItem borderLeftWidth="1px" borderRightWidth="1px" borderRadius="lg">
+                            <AccordionButton>
+                                <Box flex='1' textAlign='left'>
+                                    Skill Info
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                            <AccordionPanel>
+                                <TableContainer>
+                                    <Table variant='simple' size="sm">
+                                        <Thead>
+                                            <Tr>
+                                                <Th>Skill ID</Th>
+                                                <Th>Name</Th>
+                                                <Th>Description</Th>
+                                            </Tr>
+                                        </Thead>
+                                        <Tbody>
+                                            {(props.skills).map((skill, i) => {
+                                                return (
+                                                    <Tr>
+                                                        <Td>{skill.id}</Td>
+                                                        <Td>{skill.name}</Td>
+                                                        <Td>{skill.desc}</Td>
+                                                    </Tr>
+                                                )
+                                            })}
+                                        </Tbody>
+                                    </Table>
+                                </TableContainer>
+                            </AccordionPanel>
+                        </AccordionItem>
+                    </Accordion>
                 </Box>
             </Box>
         </Box>
@@ -74,8 +186,8 @@ export default function Main() {
     return (
         <Box bg="gray.100">
             <VStack>
-                <EmployeeCard id="00000" first="John" last="Smith" dob={employee1DOB} email="john.smith@gmail.com" skills={["Front-End", "Back-End"]} isActive={true} />
-                <EmployeeCard id="00001" first="Mike" last="Jones" dob={employee2DOB} email="mike.jones@gmail.com" skills={["Communications"]} isActive={false} />
+                <EmployeeCard id="00000" first="John" last="Smith" dob={employee1DOB} email="john.smith@gmail.com" skills={employee1skills} isActive={true} />
+                <EmployeeCard id="00001" first="Mike" last="Jones" dob={employee2DOB} email="mike.jones@gmail.com" skills={employee2skills} isActive={false} />
             </VStack>
         </Box>
     )
